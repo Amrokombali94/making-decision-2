a124f2302e4f4ef2597e66b0a89b380d78c75784
Amro Abdalla <wed.kom@hotmail.com>
2022-03-03 15:24:50 -0600

    Addition implementation working

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -9,7 +9,8 @@ var arr = [10,20,30,40,50,60]
 */
 
 //Code Here
-
+let firstItem = arr[0];
+console.log(firstItem)
 
 ////////// PROBLEM 2 //////////
 /*
@@ -17,6 +18,9 @@ var arr = [10,20,30,40,50,60]
 */
 
 //Code Here
+let lastItemRomved = arr.pop()
+
+console.log(arr)
 
 ////////// PROBLEM 3 //////////
 
@@ -29,7 +33,9 @@ var family = ['Tyler', 'Jordan', 'Ryan', 'Alice', 'Ireland'];
 */
 
 //Code Here
-
+for(i=0; i < family.length; i++){
+  console.log(family[i])
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -44,7 +50,13 @@ let evensArr = []
 */
 
 //Code Here
+for (i=0; i<nums.length; i++){
+  if(nums[i] % 2 ===0){
+    evensArr.push(nums[i])
+  }
+}
 
+console.log(evensArr)
 
 
 ////////// PROBLEM 5 //////////
@@ -58,6 +70,17 @@ var score = 74
 */
 
 //Code Here
+if(score >= 90){
+  console.log(" You grade is A")
+}else if(score < 90 && score >= 80){
+  console.log(" You grade is B")
+} else if (score < 80 && score >= 70){
+  console.log(" You grade is C")
+} else if (score < 70 && score >= 60){
+  console.log(" You grade is D")
+}else{
+  console.log(" You grade is F")
+}
 
 
 ////////// Intermediate Problems //////////
@@ -73,7 +96,9 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 */
 
 //Code Here
+let someNum = myFavoriteNumbers.splice(4,1)
 
+console.log(someNum)
 
 ////////// PROBLEM 7 //////////
 
@@ -81,7 +106,11 @@ var myFavoriteNumbers = [4,8,12,16,20,24];
 // Use an if statement to check the length of the 'myFavoriteNumbers' array. If it's less than 7, log 'There are not enough elements in this array' to the console. If the length is more than 7, reassign the value of 'someNum' to the value of the 7th element in the array. (Hint: how can you make sure that your code works for exactly 7 elements? What index do you use to get the 7th element?)
 
 //Code Here
-
+if ( myFavoriteNumbers.length < 7){
+  console.log('There are not enough elements in this array')
+} else {
+  someNum = myFavoriteNumbers[7]
+}
 
 ////////// PROBLEM 8 //////////
 
@@ -92,6 +121,12 @@ var listOfNumbers = [1,2,3,4,5,6,7,8,9,10,11,12];
 // Use a for-loop to iterate through 'listOfNumbers', checking to see if each number is divisible by 3. If it is, console.log '{number} is divisible by 3.'
 
 //Code Here
+for(i=0; i < listOfNumbers.length; i++ ){
+  if(listOfNumbers[i] % 3 === 0){
+    console.log(`${listOfNumbers[i]} is divisible by 3`)
+  }
+}
+
 
 
 ////////// PROBLEM 9 //////////
@@ -104,7 +139,9 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+for (i= letters.length - 1; i >= 0; i--){
+  console.log(letters[i])
+}
 
 ////////// Advanced Problems //////////
 
@@ -127,7 +164,25 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 
 //Code Here
 
-
+switch (letterGrade){
+  case 'A':
+    console.log('The student is doing excellently.')
+    break;
+  case 'B':
+    console.log("The student is doing well.")
+    break;
+  case 'C':
+    console.log("The student is doing alright.")
+    break;
+  case 'D':
+    console.log("The student is not doing very well.")
+    break;
+  case 'F':
+    console.log("The student is failing.")
+    break;
+  default:
+    console.log('Not an eligible grade.')
+}
 
 ////////// PROBLEM 11 //////////
 /* The famous FizzBuzz, Devmountain style!
@@ -154,3 +209,15 @@ If the letter grade is not one of the above letters, console.log 'Not an eligibl
 */
 
 //Code Here
+
+for ( i = 1; i <= 100; i++){
+  if(i % 15 === 0){
+    console.log('Devmountain')
+  } else if (i % 3 === 0){
+    console.log('Dev')
+  } else if(i %5 === 0){
+    console.log('Mountain')
+  } else {
+    console.log(i)
+  }
+}
